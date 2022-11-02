4848130349c48e27f3ab45073c2136a960617669
jonas.silva <jonas.eos@outlook.com>
2022-11-02 18:51:21 -0300

    feature: error control

--- src/server.js
+++ src/server.js
@@ -1,11 +1,35 @@
 require("express-async-errors");
 
+const AppError = require("./utils/AppErros");
 const express = require("express");
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+
+/**
+ * Manipulate error and how application works with each error,
+ *  first check if the error is a instance of AppError and thrown a error
+ *  sended by AppError instance, if isn't a AppError, then show the error
+ *  in console and return the status error in json format.
+ */
+app.use((error, _request, response, _next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: error,
+      message: error.message
+    });
+  };
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
+
 
 app.listen(port, showServerStatus);
 
